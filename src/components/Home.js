1c652b382d6f8432ad55c6b7a798edb9fb953eee
salmandanish <salmandanishmirza@hotmail.com>
2023-08-23 22:44:05 +0500

    update experience

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
             <h3 className="subtitle">Introduction</h3>
             <h3 className="title">Software Engineer</h3>
             <p className="desc">
-              I'm a Software Engineer proficient in Angular 2+, .net, SQL, COSMOS DB and Mongo DB with over 4 years of experience. I like to work with new people to get new experience.
+              I'm a Software Engineer proficient in Angular 2+, .net, SQL, COSMOS DB and Mongo DB with over 5 years of experience. I like to work with new people to get new experience.
             </p>
             <img className="sign-img" src="img/signature.png" alt="image" />
           </div>
